02cb2db32ccbc50fee74ad77843e985b55fde098
dennis <dennis.korotyaev@gmail.com>
2016-10-28 18:15:28 +0300

    chk before upload

--- algorithms/ddpg/main.py
+++ algorithms/ddpg/main.py
@@ -1,1 +1,35 @@
 # DDPG implementation for test
+from __future__ import print_function
+
+from ddpg import DDPG
+from params import *
+import numpy as np
+
+import gym
+from gym.spaces import Box
+
+from ou_noise import OUNoise
+
+
+def trainer():
+    env = gym.make(experiment)
+    steps = env.spec.timestep_limit                 # Steps per Episode via gym API call
+    assert isinstance(env.observation_space, Box), "observation space must be continuous"
+    assert isinstance(env.action_space, Box), "action space must be continuous"
+
+    # Randomly initialize critic,actor,target critic, target actor network  and replay buffer
+    agent = DDPG(env, is_batch_norm)
+    exploration_noise = OUNoise(env.action_space.shape[0])
+    counter = 0
+    reward_per_episode = 0
+    total_reward = 0
+    num_states = env.observation_space.shape[0]
+    num_actions = env.action_space.shape[0]
+    print("Number of States:", num_states)
+    print("Number of Actions:", num_actions)
+    print("Number of Steps per episode:", steps)
+    # saving reward:
+    reward_st = np.array([0])
+
+if __name__ == '__main__':
+    trainer()
---
+++ algorithms/ddpg/params.py
@@ -0,0 +1,3 @@
+episodes = 10000                    # Number of episodes to pass through train
+is_batch_norm = True                # Batch normalization is ON while True (False - batch norm is off)
+experiment = 'BipedalWalker-v2'     # Name of the Environment to use in train process
