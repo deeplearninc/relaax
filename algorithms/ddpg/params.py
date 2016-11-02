79bfedd3ad214bfb3fbe1fba49cd569dc8a8e2fb
dennis <dennis.korotyaev@gmail.com>
2016-11-02 15:54:09 +0300

    freeze to resolve memory issues

--- algorithms/ddpg/params.py
+++ algorithms/ddpg/params.py
@@ -3,7 +3,7 @@ is_batch_norm = True                # Batch normalization is ON while True (Fals
 experiment = 'BipedalWalker-v2'     # Name of the Environment to use in train process
 
 # ddpg
-REPLAY_MEMORY_SIZE = 5000          # experience replay memory buffer size [10000]
+REPLAY_MEMORY_SIZE = 10000          # experience replay memory buffer size [10000]
 BATCH_SIZE = 64                     # mini-batch size
 GAMMA = 0.99                        # discount factor
 is_grad_inverter = True             # grad descent / ascent switcher
