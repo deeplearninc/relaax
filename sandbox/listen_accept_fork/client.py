b6759580a2fbb3d5738c0454560fa303cf650fe2
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2016-11-15 15:53:44 +0300

    sandbox/listen_accept_fork/*.py

--- sandbox/listen_accept_fork/client.py
+++ sandbox/listen_accept_fork/client.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 
+import logging
 import os
 import socket
-import logging
+import time
 
 def info(message, *args):
     logging.info('%d:' + message, os.getpid(), *args)
@@ -13,7 +14,13 @@ def main():
         level=logging.INFO
     )
 
-    info('client')
+    s = socket.socket()
+    s.connect('localhost', 7000)
+
+    while True:
+        time.sleep(1)
+        s.send(bytes('hello', 'utf-8'))
+        info('client')
 
 if __name__ == '__main__':
     main()
--- sandbox/listen_accept_fork/server.py
+++ sandbox/listen_accept_fork/server.py
@@ -4,20 +4,43 @@ import os
 import socket
 import logging
 
+
 def info(message, *args):
     logging.info('%d:' + message, os.getpid(), *args)
 
-def handle_connection():
-    pass
+
+def handle_connection(c, address):
+    info('%s: start', address)
+    while True:
+        data = c.recv(1024)
+        if data is None:
+            break
+        s = data.decode('utf-8')
+        info('%s: received %s', address, repr(s))
+    info('%s: stop', address)
+
 
 def main():
     logging.basicConfig(
         format='%(asctime)s:%(levelname)s: %(message)s',
         level=logging.INFO
     )
-    os.fork()
-    info('server')
-    pass
+
+    s = socket.socket()
+    s.bind('localhost', 7000)
+    s.listen(100)
+
+    while True:
+        c, address = s.accept()
+        pid = os.fork()
+        if pid == 0:
+            s.close()
+            handle_connection(c, address)
+            c.close()
+            break
+        c.close()
+
+    s.close()
 
 if __name__ == '__main__':
     main()
