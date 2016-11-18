7826ba685ce40a9afcc4e337ebaffacfad97ad9c
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2016-11-19 01:29:25 +0300

    sandbox/cluster3/run_cluster.sh: some clean up

--- sandbox/cluster3/run_cluster.sh
+++ sandbox/cluster3/run_cluster.sh
@@ -6,21 +6,14 @@ source activate server&&exec python master.py &>out/master &
 PIDS+=($!)
 sleep 1
 
-# for i in `seq 0 $((N - 1))`;
-for i in `seq 0 0`;
-do
-    # echo worker $i
-    # source activate server&&exec gunicorn -k flask_sockets.worker -b localhost:$((8000 + i)) "worker:main($i)" &>out/worker_$i &
-    echo worker
-    source activate server&&exec python worker.py &>out/worker &
-    PIDS+=($!)
-    sleep 1
-done
+echo worker
+source activate server&&exec python worker.py &>out/worker &
+PIDS+=($!)
+sleep 1
 
 for i in `seq 0 $((N - 1))`;
 do
     echo client $i
-    # source activate client&&exec python ../../clients/rl_client_ale.py --host localhost --port $((8000 + i)) --seed $i &>out/client_$i &
     source activate client&&exec python ../../clients/rl_client_ale.py --agent localhost:7000 --seed $i &>out/client_$i &
     PIDS+=($!)
     sleep 1
