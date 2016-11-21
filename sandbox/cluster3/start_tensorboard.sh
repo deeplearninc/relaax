050d3ff0486e9ca0478db88d8cd82897525e91b5
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2016-11-21 11:04:51 +0300

    sandbox/cluster3/start_tensorboard.sh: log dir was changed

--- sandbox/cluster3/start_tensorboard.sh
+++ sandbox/cluster3/start_tensorboard.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-n=$1
+N=$1
 logdir=()
-for i in `seq 0 $((n - 1))`;
+for i in `seq 0 $((N - 1))`;
 do
     if ((i)); then
         logdir+=","
     fi
-    logdir+="worker_$i:logs/boxing_a3c_1threads/worker_$i"
+    logdir+="worker_$i:logs/boxing_a3c_${N}_agents/worker_$i"
 done    
 echo $logdir
 
