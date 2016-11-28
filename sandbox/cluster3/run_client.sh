242417d777ed67dab744ad6ab67155139abc9565
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2016-11-28 20:52:23 +0300

    convenient way to control number of working clients.

--- sandbox/cluster3/run_client.sh
+++ sandbox/cluster3/run_client.sh
@@ -1,3 +1,1 @@
-AGENT=35.164.194.133:80
-
-source activate client&&exec python ../../clients/rl_client_ale.py --agent $AGENT --seed 0
+exec python ../../clients/rl_client_ale.py --agent $1 --seed $2 &>out/client_$2
