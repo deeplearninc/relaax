gradients --> need fix

server/algorithms/a3c/agent.py
@@ -19,8 +19,7 @@ class Agent(object):
 
         with tf.device(kernel):
             self._local_network = game_ac_network \
-                .make_full_network(params, 0) \
-                .prepare_loss(params)
+                .make_full_network(params, 0)
 
         # TODO: don't need accum trainer anymore with batch
         trainer = accum_trainer.AccumTrainer(kernel)
@@ -220,7 +219,17 @@ class Agent(object):
 
         start_time = time.time()
 
-        self._master.apply_gradients(self._session.run(self._accum_grad_list))
+        if True:
+            loss, vars = self._session.run([self._local_network.total_loss, self._local_network.get_vars()],
+                                           feed_dict={
+                                               self._local_network.s: batch_si,
+                                               self._local_network.a: batch_a,
+                                               self._local_network.td: batch_td,
+                                               self._local_network.r: batch_R
+                                           })
+            self._master.apply_gradients(loss, vars)
+        else:
+            self._master.apply_gradients(self._session.run(self._accum_grad_list))
 
         if (self.local_t % 100) == 0:
             print("TIMESTEP", self.local_t)

server/algorithms/a3c/game_ac_network.py
@@ -100,19 +100,26 @@ class _GameACFFNetworkShared(_GameACNetwork):
         ])
 
         self.gradients = [tf.placeholder(v.dtype, v.get_shape()) for v in self.values]
+        gradients = [tf.Variable(initial_value=v.initialized_value()) for v in self.values]
+        self.assign_grads = tf.group(*[tf.assign(g, sg) for g, sg in zip(gradients, self.gradients)])
 
         self.learning_rate_input = tf.placeholder(tf.float32)
 
         if True:
+            self.loss = tf.placeholder(tf.float32)
+            loss = tf.Variable(0.0)
+            self.assign_loss = tf.assign(loss, self.loss)
             optimizer = tf.train.RMSPropOptimizer(
                 learning_rate=self.learning_rate_input,
                 decay=params.RMSP_ALPHA,
                 momentum=0.0,
                 epsilon=params.RMSP_EPSILON
             )
-            grads_and_vars = optimizer.compute_gradients(self.total_loss, self.gradients)
+            grads_and_vars = optimizer.compute_gradients(loss, gradients)
+            print(grads_and_vars)
             grads_and_vars = [[tf.clip_by_norm(grad, params.GRAD_NORM_CLIP), var]
                               for grad, var in grads_and_vars if grad is not None]
+            print(len(grads_and_vars))
             self.apply_gradients = optimizer.apply_gradients(grads_and_vars)
         else:
             applier = rmsprop_applier.RMSPropApplier(