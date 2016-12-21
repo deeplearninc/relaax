7ade420a8b64af4708358c654e8cbaa70d2896b3
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2016-12-21 22:15:37 +0300

    remove client logic from socket_loop

--- sandbox/cluster1/client.sh
+++ sandbox/cluster1/client.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python ../../relaax/client/ale/main.py --agent localhost:7001 --seed 0 --ale ../../../Arcade-Learning-Environment --rom ../../../atari-games/boxing.bin
+python ../../relaax/client/ale/main.py --rlx-server localhost:7001 --seed 0 --ale ../../../Arcade-Learning-Environment --rom ../../../atari-games/boxing.bin
