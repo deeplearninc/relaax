6be3791d30c53761c41407ae0683999c407d251c
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2016-12-26 19:45:46 +0300

    clean relaax/server/rlx_server/__init__.py

--- relaax/server/rlx_server/main.py
+++ relaax/server/rlx_server/main.py
@@ -1,4 +1,9 @@
+from __future__ import print_function
+
+import argparse
+import logging
 import os
+import ruamel.yaml
 import sys
 
 sys.path.append(os.path.normpath(os.path.join(
@@ -6,6 +11,39 @@ sys.path.append(os.path.normpath(os.path.join(
     '../../..'
 )))
 
-import relaax.server.rlx_server
+import relaax.server.rlx_server.server
 
-relaax.server.rlx_server.main()
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--log-level',
+    type=str,
+    default='WARNING',
+    choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'),
+    help='logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)'
+)
+parser.add_argument('--config', type=str, default=None, help='parameters YAML file')
+parser.add_argument('--bind', type=str, default=None, help='address to serve (host:port)')
+parser.add_argument('--parameter_server', type=str, default=None, help='parameter server address (host:port)')
+parser.add_argument('--log-dir', type=str, default=None, help='TensorBoard log directory')
+parser.add_argument('--timeout', type=float, default=120, help='Agent stops on game reset after given timeout')
+args = parser.parse_args()
+
+log_level = getattr(logging, args.log_level.upper(), None)
+if not isinstance(log_level, int):
+    raise ValueError('Invalid log level: %s' % loglevel)
+logging.basicConfig(
+    format='%(asctime)s:%(levelname)s:%(message)s',
+    level=log_level
+)
+
+with open(args.config, 'r') as f:
+    yaml = ruamel.yaml.load(f, Loader=ruamel.yaml.Loader)
+
+relaax.server.rlx_server.server.run(
+    bind_address=args.bind,
+    yaml=yaml,
+    parameter_server=args.parameter_server,
+    log_dir=args.log_dir,
+    timeout=args.timeout
+)
