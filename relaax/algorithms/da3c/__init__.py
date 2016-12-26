6e86c050c83e0d5ba343ca1626a1d06f1ac94f84
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2016-12-26 20:34:26 +0300

    clean relaax/algorithms/da3c/parameter_server/__init__.py

--- relaax/algorithms/da3c/__init__.py
+++ relaax/algorithms/da3c/__init__.py
@@ -1,4 +1,4 @@
 from .common.config import Config
-from .parameter_server import ParameterServer
+from .parameter_server.parameter_server import ParameterServer
 from .agent import Agent
 from .common.bridge import ParameterServerStub, ParameterServerService, start_parameter_server
