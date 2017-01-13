e7f6b92bce900ab03e702b9d357f03dc5d36c069
dennis <nemiroff.den@gmail.com>
2017-01-13 23:14:48 +0300

    add depends for lab docker & fix lvl for lab

--- environments/DeepMind_Lab/startup.sh
+++ environments/DeepMind_Lab/startup.sh
@@ -4,7 +4,7 @@ mkdir -p /var/run/sshd
 
 # create an ubuntu user
 # PASS=`pwgen -c -n -1 10`
-PASS=ubuntu
+PASS=relaax
 # echo "Username: ubuntu Password: $PASS"
 id -u ubuntu &>/dev/null || useradd --create-home --shell /bin/bash --user-group --groups adm,sudo ubuntu
 echo "ubuntu:$PASS" | chpasswd
