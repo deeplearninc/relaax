44973e93bd5b39e5fd2343cab1ff56531d2c5ca4
dennis <nemiroff.den@gmail.com>
2017-01-18 20:44:14 +0300

    update run scripts for gym

--- environments/OpenAI_Gym/start.sh
+++ environments/OpenAI_Gym/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# env vars
+# Environment variables, which you can set to docker with -e
 : ${VNC_PASS:=relaax}
 : ${X11_DISPLAY_NO:=99}
 : ${X11_WINDOW_MANAGER:=ratpoison}
@@ -9,4 +9,9 @@
 mkdir ~/.vnc
 x11vnc -storepasswd "$VNC_PASS" ~/.vnc/passwd
 
-xvfb-run -n "$X11_DISPLAY_NO" -s "-screen 0 1280x720x24" ./x.sh $1 $2 "$X11_WINDOW_MANAGER"
+if [ -z "$3" ]
+then
+    xvfb-run -n "$X11_DISPLAY_NO" -s "-screen 0 1280x720x24" ./x.sh $1 $2 "$X11_WINDOW_MANAGER"
+else
+    xvfb-run -n "$X11_DISPLAY_NO" -s "-screen 0 1280x720x24" ./x.sh $1 $2 "$X11_WINDOW_MANAGER" $3
+fi
