2e6baa9bf0d1df6974800549f7b8b9b3354f4d52
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-02-04 00:17:21 +0300

    save on time and global step

--- relaax/server/parameter_server/main.py
+++ relaax/server/parameter_server/main.py
@@ -25,6 +25,8 @@ def main():
     parser.add_argument('--bind', type=str, default=None, help='address to serve (host:port)')
     parser.add_argument('--checkpoint-dir', type=str, default=None, help='TensorFlow checkpoint directory')
     parser.add_argument('--checkpoint-aws-s3', nargs=2, type=str, default=None, help='AWS S3 bucket and key for TensorFlow checkpoint')
+    parser.add_argument('--checkpoint-time-interval', type=int, default=None, help='save on regular intervals in seconds')
+    parser.add_argument('--checkpoint-global-step-interval', type=int, default=None, help='save on regular intervals in global steps')
     parser.add_argument('--metrics-dir', type=str, default=None, help='TensorBoard metrics directory')
     parser.add_argument('--aws-keys', type=str, default=None, help='YAML file containing AWS access and secret keys')
     args = parser.parse_args()
@@ -42,21 +44,29 @@ def main():
     if 'relaax-parameter-server' in yaml:
         cmdl = yaml['relaax-parameter-server']
 
-        if args.log_level is None and '--log-level' in cmdl:
-            args.log_level = cmdl['--log-level']
-        if args.bind is None and '--bind' in cmdl:
-            args.bind = cmdl['--bind']
-        if args.checkpoint_dir is None and '--checkpoint-dir' in cmdl:
-            args.checkpoint_dir = cmdl['--checkpoint-dir']
-        if args.metrics_dir is None and '--metrics-dir' in cmdl:
-            args.metrics_dir = cmdl['--metrics-dir']
-        if args.checkpoint_aws_s3 is None and '--checkpoint-aws-s3' in cmdl:
-            args.checkpoint_aws_s3 = cmdl['--checkpoint-aws-s3']
+        for arg in [
+            '--log-level',
+            '--bind',
+            '--checkpoint-dir',
+            '--checkpoint-time-interval',
+            '--checkpoint-global-step-interval',
+            '--metrics-dir',
+            '--checkpoint-aws-s3'
+        ]:
+            if arg in cmdl:
+                assert arg[0:2] == '--'
+                key = arg[2:].replace('-', '_')
+                if vars(args)[key] is None:
+                    vars(args)[key] = cmdl[arg]
 
     relaax.server.parameter_server.server.run(
         yaml=yaml['algorithm'],
         bind=args.bind,
         saver=_saver(args),
+        intervals={
+            'checkpoint_time_interval': args.checkpoint_time_interval,
+            'checkpoint_global_step_interval': args.checkpoint_global_step_interval
+        },
         metrics=_Metrics(args.metrics_dir)
     )
 
