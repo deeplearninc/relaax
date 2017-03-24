fb678d64df7c3edbd671a2c0554506d1e3a7b921
dennis <nemiroff.den@gmail.com>
2017-03-24 21:02:12 +0300

    feat: split computing of gamma_operator from DiscountedReward class to get lightweight constructor

--- relaax/common/algorithms/rewards.py
+++ relaax/common/algorithms/rewards.py
@@ -47,7 +47,75 @@ def discounted_reward(rewards, gamma, normalize=True, constraint=False):
     return discounted_r
 
 
+def get_gamma_operator(gamma_, batch_size_):
+    """Computes gamma_operator wrt batch (buffer) size & gamma.
+
+    Args:
+        gamma_ (float): Discount factor.
+        batch_size_ (int): The upper limit of the powers to compute.
+
+    Returns:
+        gamma_operator (numpy.ndarray): triangle matrix with powers of gamma.
+
+    Examples
+    ----------
+    >>> gamma = 0.95
+    >>> batch_size = 3
+    >>> print(get_gamma_operator(gamma, batch_size))
+    >>> [[1.    0.95    0.9025]
+    >>>  [0.    1.      0.95  ]
+    >>>  [0.    0.      1.    ]
+    """
+    # create array of gamma's powers from [0..batch_size)
+    powers = np.power(np.ones(batch_size_) * gamma_, np.arange(batch_size_))
+
+    # create & fill upper triangle ladder matrix
+    gamma_operator = np.zeros((batch_size_, batch_size_))
+    for idx in moves.xrange(batch_size_):
+        gamma_operator[idx, idx:] = powers[:batch_size_ - idx]
+
+    return gamma_operator
+
+
 class DiscountedReward(object):
+    def __init__(self, gamma_operator_):
+        # initialize tensorflow variable with incoming upper triangle ladder matrix
+        gamma_operator = tf.Variable(gamma_operator_, name='gamma_operator', dtype=np.float32)
+
+        # define interfaces to feed in: rewards array and its length
+        self._ph_rewards = tf.placeholder(tf.float32, [None, 1], name='ph_rewards')
+        self._length = tf.placeholder(tf.int32, [1], name='slice_index')
+
+        # operation to compute a column vector of discounted rewards by appropriate matrix multiplication
+        self._compute = tf.matmul(gamma_operator[:self._length, :self._length], self._ph_rewards)
+
+        # additional operations for normalization the resulting rewards wrt its mean and std
+        mean_centered = self._compute - tf.reduce_mean(self._compute)
+        epsilon = tf.constant(1e-20)  # to prevent zero dividing when all rewards are equal to 0
+        self._normalize = mean_centered / tf.sqrt(tf.nn.moments(mean_centered, axes=[0])[1] + epsilon)
+
+    def __call__(self, sess, rewards, normalize=False):
+        """Returns computed discounted rewards as a column vector.
+
+        Args:
+            sess : Tensorflow session.
+            rewards (numpy.ndarray): Column vector with shape == (batch_size, 1).
+            normalize (bool): if True -> additionally normalize the computed rewards.
+
+        Returns:
+            discounted rewards (numpy.ndarray): Has the same shape as input Args:rewards.
+        """
+
+        # dictionary to pass in respective tensorflow op, where we additionally
+        # retrieves the batch_size from the 1st dimension of incoming column vector
+        feeds = {self._ph_rewards: rewards, self._length: rewards.shape[0]}
+
+        if normalize:
+            return sess.run(self._normalize, feed_dict=feeds)
+        return sess.run(self._compute, feed_dict=feeds)
+
+
+class DiscountedRewardFull(object):
     """Take numpy (column) array of rewards and compute discounted rewards.
 
     Examples
@@ -55,7 +123,7 @@ class DiscountedReward(object):
     >>> rewards = [1, 0, 0, 1, 0,
     >>>            0, 0, 1, 1, 1]
     >>> gamma = 0.95
-    >>> dr_caller = DiscountedReward(gamma, len(rewards))
+    >>> dr_caller = DiscountedRewardFull(gamma, len(rewards))
     >>> with tf.Session() as sess:
     >>>    sess.run(tf.variables_initializer(tf.global_variables()))
     >>>    # we have to rebuild rewards list to numpy column vector with `np.vstack`
@@ -111,7 +179,7 @@ class DiscountedReward(object):
         self._normalize = mean_centered / tf.sqrt(tf.nn.moments(mean_centered, axes=[0])[1] + epsilon)
 
     def __call__(self, sess, rewards, normalize=False):
-        """Returns computed discounted rewards as column vector.
+        """Returns computed discounted rewards as a column vector.
 
         Args:
             sess : Tensorflow session.
