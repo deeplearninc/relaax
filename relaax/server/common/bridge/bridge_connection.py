031f02a811724a7bcd93ac167e8c620ee1308dc7
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-03-24 12:32:06 +0300

    relaax/server/common/bridge/bridge_connection.py: change server_url to host-port n BridgeConnection.

--- relaax/server/common/bridge/bridge_connection.py
+++ relaax/server/common/bridge/bridge_connection.py
@@ -6,8 +6,8 @@ from bridge_serializer import BridgeSerializer
 
 
 class BridgeConnection(object):
-    def __init__(self, server_url):
-        self._stub = bridge_pb2.BridgeStub(grpc.insecure_channel(server_url))
+    def __init__(self, server):
+        self._stub = bridge_pb2.BridgeStub(grpc.insecure_channel('%s:%d' % server))
 
     def run(self, ops, feed_dict):
         messages = BridgeSerializer.serialize([ops, feed_dict])
