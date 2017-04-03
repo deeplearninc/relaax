ca0439ed841de88dbc4d0585c0cb5accaeabe7a4
vlad-dplrn <vlad@dplrn.com>
2017-04-03 13:43:50 -0700

    design proposal

--- algorithms/policy_gradient/pg_model.py
+++ algorithms/policy_gradient/pg_model.py
@@ -27,20 +27,21 @@ class PolicyModel(Subgraph):
     def build(self):
         # Build TF graph
         weights = Weights()
-        self.shared_weights = Placeholders(variables=weights)
-        self.assign_weights = Assign(weights, self.shared_weights)
-        
-        self.state = Placeholder((None, config.state_size))
-        self.action = Placeholder((None, config.action_size))
-        self.discounted_reward = Placeholder((None, 1))
-        self.policy = FullyConnected(state=self.state, weights=weights)
-        loss = Loss(
-            action=self.action,
-            policy=self.policy,
-            discounted_reward=self.discounted_reward
-        )
-        self.partial_gradients = Gradients(loss=loss, variables=weights)
-        
+        self.ph_weights = self.ph_weights
+        self.assign_weights = self.assign_weights
+
+        self.ph_state = Placeholder((None, config.state_size))
+        self.policy = FullyConnected(state=self.ph_state, weights=weights.variables)
+
+        self.ph_action = Placeholder((None, config.action_size))
+        self.ph_action_probability = Placeholder(...)
+        self.ph_discounted_reward = Placeholder((None, 1))
+        self.partial_gradients = PartialGradients(
+            ph_action=self.ph_action,
+            ph_action_probability=self.ph_action_probability,
+            ph_discounted_reward=self.ph_discounted_reward, 
+            weights=weights.variables)
+
         self.initialize = Initialize()
 
 
