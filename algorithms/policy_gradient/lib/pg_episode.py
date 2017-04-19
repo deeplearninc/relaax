c5b3fe9e4c13536fb552ad78e0a9dfcc5a39f7e6
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-04-19 16:05:49 +0300

    algorithms/policy_gradient/lib/pg_episode.py: push full experiences only

--- algorithms/policy_gradient/lib/pg_episode.py
+++ algorithms/policy_gradient/lib/pg_episode.py
@@ -12,6 +12,8 @@ class PGEpisode(object):
         self.ps = parameter_server
         self.session = session.Session(pg_model.PolicyModel())
         self.reset()
+        self.last_state = None
+        self.last_action = None
 
     @property
     def experience_size(self):
@@ -22,15 +24,10 @@ class PGEpisode(object):
         self.episode.begin()
 
     def step(self, reward, state, terminal):
-        if state is None:
-            action = None
-        else:
-            action = self.action_from_policy(state)
-            assert action is not None
-
-        self.episode.step(reward=reward, state=state, action=action)
-
-        return action
+        if reward is not None:
+            self.push_experience(reward)
+        assert (state is None) == terminal
+        return self.keep_state_and_action(state)
 
     def end(self):
         experience = self.episode.end()
@@ -42,7 +39,30 @@ class PGEpisode(object):
 
     # Helper methods
 
-    # reload policy weights from PS
+    def push_experience(self, reward):
+        assert self.last_state is not None
+        assert self.last_action is not None
+
+        self.episode.step(
+            reward=reward,
+            state=self.last_state,
+            action=self.last_action
+        )
+        self.last_state = None
+        self.last_action = None
+
+    def keep_state_and_action(self, state):
+        assert self.last_state is None
+        assert self.last_action is None
+
+        self.last_state = state
+        if state is None:
+            self.last_action = None
+        else:
+            self.last_action = self.action_from_policy(state)
+            assert self.last_action is not None
+        return self.last_action
+
     def load_shared_parameters(self):
         self.session.op_assign_weights(values=self.ps.op_get_weights())
 
