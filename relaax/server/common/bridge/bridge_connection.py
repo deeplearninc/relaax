2b5c8b2424405f9f55bc71ca91fc6149c638847f
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-05-12 20:14:07 +0300

    relaax/server/common/bridge/bridge_connection.py: make connection lazy

--- relaax/server/common/bridge/bridge_connection.py
+++ relaax/server/common/bridge/bridge_connection.py
@@ -10,38 +10,45 @@ from . import bridge_message
 
 class BridgeConnection(object):
     def __init__(self, server):
-        self.__stub = bridge_pb2.BridgeStub(grpc.insecure_channel('%s:%d' % server))
-        self.__stub.Init(bridge_pb2.NullMessage())
-        self.session = BridgeSession(self.__stub)
-        self.metrics = BridgeMetrics(self.__stub)
+        self._server = server
+        self.session = BridgeSession(self)
+        self.metrics = BridgeMetrics(self)
+        self._stub = None
+
+    @property
+    def stub(self):
+        if self._stub is None:
+            self._stub = bridge_pb2.BridgeStub(grpc.insecure_channel('%s:%d' % self._server))
+            self._stub.Init(bridge_pb2.NullMessage())
+        return self._stub
 
 
 class BridgeSession(object):
-    def __init__(self, stub):
-        self.__stub = stub
+    def __init__(self, connection):
+        self.__connection = connection
 
     def __getattr__(self, name):
-        return BridgeSessionMethod(self.__stub, name)
+        return BridgeSessionMethod(self.__connection, name)
 
 
 class BridgeSessionMethod(object):
-    def __init__(self, stub, op):
-        self.stub = stub
+    def __init__(self, connection, op):
+        self.connection = connection
         self.op = op
 
     def __call__(self, **kwargs):
         feed_dict = kwargs
         messages = bridge_message.BridgeMessage.serialize([self.op, feed_dict])
-        result = self.stub.Run(messages)
+        result = self.connection.stub.Run(messages)
         return bridge_message.BridgeMessage.deserialize(result)
 
 
 class BridgeMetrics(metrics.Metrics):
-    def __init__(self, stub):
-        self.__stub = stub
+    def __init__(self, connection):
+        self.__connection = connection
 
     def scalar(self, name, y, x=None):
         kwargs = {'name': name, 'y': y}
         if x is not None:
             kwargs['x'] = bridge_pb2.ScalarMetric.Arg(value=x)
-        self.__stub.StoreScalarMetric(bridge_pb2.ScalarMetric(**kwargs))
+        self.__connection.stub.StoreScalarMetric(bridge_pb2.ScalarMetric(**kwargs))
