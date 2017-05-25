fixes for policy gradient

relaax/algorithms/policy_gradient/lib/pg_episode.py
@@ -1,4 +1,7 @@
 from builtins import object
+
+import numpy as np
+
 from relaax.server.common import session
 from relaax.common.algorithms.lib import episode
 from relaax.common.algorithms.lib import utils
@@ -28,8 +31,10 @@ class PGEpisode(object):
         if reward is not None:
             self.push_experience(reward)
         assert (state is None) == terminal
-        if state is not None:
-            state = [[i] for i in state]
+        state = np.asarray(state)
+        if state.size == 0:
+            state = np.asarray([0])
+        state = np.reshape(state, state.shape + (1,))
         action = self.get_action(state)
         self.keep_state_and_action(state, action)
         return action
@@ -75,7 +80,6 @@ class PGEpisode(object):
 
     def action_from_policy(self, state):
         assert state is not None
-
         probabilities, = self.session.op_get_action(state=[state])
         return utils.choose_action_descrete(probabilities, self.exploit)
 

relaax/common/algorithms/lib/layer.py
@@ -33,7 +33,10 @@ class Border(object):
 
 class BaseLayer(subgraph.Subgraph):
     def build_graph(self, x, shape, transformation, activation):
-        d = 1.0 / np.sqrt(np.prod(shape[:-1]))
+        d = 1.0
+        p = np.prod(shape[:-1])
+        if p != 0:
+            d = 1.0 / np.sqrt(p)
         initializer = graph.RandomUniformInitializer(minval=-d, maxval=d)
         W = graph.Variable(initializer(np.float32, shape)).node
         b = graph.Variable(initializer(np.float32, shape[-1:])).node
@@ -105,8 +108,11 @@ def Actor(head, output):
 
 class Input(subgraph.Subgraph):
     def build_graph(self, input):
+        input_shape = input.shape
+        if np.prod(input.shape) == 0:
+            input_shape = [1]
         self.ph_state = graph.Placeholder(np.float32,
-                shape=[None] + input.shape + [input.history])
+                shape=[None] + input_shape + [input.history])
 
         descs = []
         if input.use_convolutions: