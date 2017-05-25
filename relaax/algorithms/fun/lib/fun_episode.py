592658ea95b680607f930e7da7dfc67494fb68e6
dennis <nemiroff.den@gmail.com>
2017-05-25 19:10:51 +0300

    upd: split episode in methods

--- relaax/algorithms/fun/lib/fun_episode.py
+++ relaax/algorithms/fun/lib/fun_episode.py
@@ -58,20 +58,7 @@ class FuNEpisode(object):
                 self.cur_c += 1  # can replace
 
             if not terminal:
-                z_t = self.session.op_get_zt(ph_state=[state])
-                s_t = self.session.self.op_get_st(ph_perception=z_t)[0]
-
-                cur_st = s_t - self.st_buffer.data[-self.cur_c:, :]
-                cur_st_norm = \
-                    np.maximum(np.linalg.norm(cur_st, axis=1), self.eps)
-                st_normed = (cur_st.transpose() / cur_st_norm).transpose()
-
-                cur_goal = self.goal_buffer.data[-self.cur_c:, :]
-                cur_goal_norm = \
-                    np.maximum(np.linalg.norm(cur_goal, axis=1), self.eps)
-                goals_normed = cur_goal.transpose() / cur_goal_norm
-
-                cosine = np.dot(st_normed, goals_normed)
+                cosine = self.get_cosine(state)
                 reward_i = sum(cosine.diagonal()) / self.cur_c
             else:
                 reward_i = 0
@@ -82,19 +69,7 @@ class FuNEpisode(object):
 
         if state is not None:   # except terminal
             self.states.append(state)   # also as first state
-            zt_inp = self.session.op_get_zt(ph_state=[state])
-            self.last_zt_inp, = zt_inp
-
-            goal, m_value, s_t, lstm_state = self.session.op_get_goal_value_st(
-                    ph_perception=zt_inp,
-                    ph_initial_lstm_state=self.session.op_get_lstm_state,
-                    ph_step_size=[1])
-            self.session.op_assign_lstm_state(ph_variable=lstm_state)
-
-            self.last_m_value, = m_value
-            self.goal_buffer.extend(goal[0])
-            self.last_goal = self.goal_buffer.get_sum()
-            self.st_buffer.extend(s_t[0])
+            self.manager_step(state)    # add manager's lasts & extend buffers
 
         assert self.last_action is None
         assert self.last_value is None
@@ -147,6 +122,38 @@ class FuNEpisode(object):
         self.worker_start_lstm_state = self.session.op_get_lstm_state
         self.manager_start_lstm_state = self.session.op_get_lstm_state
 
+    def get_cosine(self, state):
+        z_t = self.session.op_get_zt(ph_state=[state])
+        s_t = self.session.self.op_get_st(ph_perception=z_t)[0]
+
+        cur_st = s_t - self.st_buffer.data[-self.cur_c:, :]
+        cur_st_norm = \
+            np.maximum(np.linalg.norm(cur_st, axis=1), self.eps)
+        st_normed = (cur_st.transpose() / cur_st_norm).transpose()
+
+        cur_goal = self.goal_buffer.data[-self.cur_c:, :]
+        cur_goal_norm = \
+            np.maximum(np.linalg.norm(cur_goal, axis=1), self.eps)
+        goals_normed = cur_goal.transpose() / cur_goal_norm
+
+        cosine = np.dot(st_normed, goals_normed)
+        return cosine
+
+    def manager_step(self, state):
+        zt_inp = self.session.op_get_zt(ph_state=[state])
+        self.last_zt_inp, = zt_inp
+
+        goal, m_value, s_t, lstm_state = self.session.op_get_goal_value_st(
+            ph_perception=zt_inp,
+            ph_initial_lstm_state=self.session.op_get_lstm_state,
+            ph_step_size=[1])
+        self.session.op_assign_lstm_state(ph_variable=lstm_state)
+
+        self.last_m_value, = m_value
+        self.goal_buffer.extend(goal[0])
+        self.last_goal = self.goal_buffer.get_sum()
+        self.st_buffer.extend(s_t[0])
+
     def get_action_and_value(self):
         if len(self.states) == 0:
             self.last_action = None
@@ -184,25 +191,29 @@ class FuNEpisode(object):
         value, = value
         return utils.choose_action(probabilities), value
 
-    def compute_gradients(self, experience):
+    def get_discounted_rewards(self, experience):
         r = ri = 0.0
         if self.last_value is not None:
             r = self.last_value
             ri = self.last_m_value
-
-        # shift to cfg.c = 10  (could be at the end method)
 
         reward = experience['reward']
         m_reward = experience['reward_i']
         discounted_reward = np.zeros_like(reward, dtype=np.float32)
         m_discounted_reward = np.zeros_like(m_reward, dtype=np.float32)
 
-        # compute and accumulate gradients
+        # compute discounted rewards
         for t in reversed(range(len(reward))):
             r = reward[t] + cfg.worker_gamma * r
             ri = m_reward[t] + cfg.manager_gamma * ri
             discounted_reward[t] = r
             m_discounted_reward[t] = ri
+
+        return discounted_reward, m_discounted_reward
+
+    def compute_gradients(self, experience):
+        discounted_reward, m_discounted_reward = \
+            self.get_discounted_rewards(experience)
 
         step_sz = len(experience['action'])
         st_diff = self.st_buffer.get_diff(part=step_sz)
