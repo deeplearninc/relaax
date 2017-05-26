7356254a87c144a2dad0ed88d8789ef41f90a8d7
vlad-dplrn <vlad@dplrn.com>
2017-05-25 23:00:19 -0700

    added simple-exchange sample and removed old files

--- sample/algorithms/sample/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from .sample_agent import SampleAgent as Agent
-from .sample_parameter_server import SampleParameterServer as ParameterServer
-
-__all__ = [Agent, ParameterServer]
