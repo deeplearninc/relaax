b2571ea13361446e072994512c476ad40eb8bd39
dennis <nemiroff.den@gmail.com>
2017-06-05 19:20:40 +0300

    add: lua custom map scripts & readme

---
+++ relaax/templates/environments/deepmind-lab/read_map.lua
@@ -0,0 +1,31 @@
+local make_map = require 'common.make_map'
+local pickups = require 'common.pickups'
+local api = {}
+
+function api:start(episode, seed)
+  make_map.seedRng(seed)
+  api._count = 0
+end
+
+function api:commandLine(oldCommandLine)
+  return make_map.commandLine(oldCommandLine)
+end
+
+function api:createPickup(className)
+  return pickups.defaults[className]
+end
+
+function readFile(file)
+    local f = io.open(file, "rb")
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
+function api:nextMap()
+  api._count = api._count + 1
+  map = readFile('t_maze')
+  return make_map.makeMap("t_maze_" .. api._count, map)
+end
+
+return api
