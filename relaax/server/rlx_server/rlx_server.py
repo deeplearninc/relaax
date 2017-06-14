7e8d5f02d42f8ae13069bfebcb29bbf4d11b488b
vlad-dplrn <vlad@dplrn.com>
2017-06-14 15:05:13 -0700

    moved loading algorithm to worker

--- relaax/server/rlx_server/rlx_server.py
+++ relaax/server/rlx_server/rlx_server.py
@@ -6,48 +6,14 @@ import traceback
 # do it as early as possible
 from .rlx_port import RLXPort
 from .rlx_config import options
-from relaax.server.common.algorithm_loader import AlgorithmLoader
 
 log = logging.getLogger(__name__)
 
 
 class RLXServer():
 
-    @staticmethod
-    def preload_algorithm():
-        try:
-            options.Agent = AlgorithmLoader.load_agent(
-                    options.algorithm_path, options.algorithm_name)
-
-        except Exception:
-            log.critical("Can't load algorithm module")
-            log.error(traceback.format_exc())
-            exit()
-
-    @staticmethod
-    def preload_protocol():
-        try:
-            name = options.protocol_name
-
-            if name == 'rawsocket':
-                log.debug("Loading protocol over raw socket")
-                from .rlx_protocol.rawsocket import rlx_protocol as protocol
-                options.protocol = protocol
-            elif name == 'twisted':
-                log.debug("Loading protocol on twisted")
-                from .rlx_protocol.twisted import rlx_protocol as protocol
-                options.protocol = protocol
-            else:
-                raise
-
-        except Exception:
-            log.critical("Can't load protocol")
-            exit()
-
     @classmethod
     def start(cls):
-        cls.preload_protocol()
-        cls.preload_algorithm()
         log.info("Starting RLX server on %s:%d" % options.bind)
         log.info("Expecting parameter server on %s:%d" % options.parameter_server)
         RLXPort.listen(options.bind)
