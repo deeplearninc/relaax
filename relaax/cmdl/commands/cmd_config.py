b75630debaaa04ecfce5d009cd72cff9580ef93f
vlad-dplrn <vlad@dplrn.com>
2017-06-15 17:02:03 -0700

    added list of all available algorithm configurations (relaax config)

--- relaax/cmdl/commands/cmd_config.py
+++ relaax/cmdl/commands/cmd_config.py
@@ -1,15 +1,13 @@
 from __future__ import print_function
-from builtins import str
 from builtins import object
 
 import os
-import sys
 import click
-import ruamel.yaml
 
 from relaax.cmdl.cmdl import pass_context
-from relaax.cmdl.cmdl import ALGORITHMS, ALGORITHMS_META, ENVIRONMENTS, ENVIRONMENTS_META
+from relaax.cmdl.cmdl import ENVIRONMENTS, ENVIRONMENTS_META
 from relaax.cmdl.utils.backup import Backup
+from relaax.cmdl.utils.commented_yaml import CommentedYaml
 
 
 class CmdConfig(object):
@@ -24,26 +22,23 @@ class CmdConfig(object):
 
     def apply(self):
         if self.algorithm is None and self.environment is None:
-            self.ctx.log('Nothing to do, exiting...')
-            return
-        if self.algorithm is not None:
-            self._configure(self.algorithm,
-                            'algorithm',
-                            '../../templates/configs/%s.yaml' % self.algorithm,
-                            self.set_algorithm_path)
-        if self.environment is not None:
-            self._configure(self.environment,
-                            'environment',
-                            '../../templates/environments/%s/app.yaml' % self.environment)
+            CmdConfig.list_configurations(self.ctx)
+        else:
+            if self.algorithm is not None:
+                self._configure(self.algorithm,
+                                'algorithm',
+                                '../../templates/configs/%s.yaml' % self.algorithm,
+                                self.set_algorithm_path)
+            if self.environment is not None:
+                self._configure(self.environment,
+                                'environment',
+                                '../../templates/environments/%s/app.yaml' % self.environment)
 
     def _configure(self, template_name, block_name, template_path, set_algorithm_path=False):
-        app_config = self._read_yaml(self.config, 'Sorry, can\'t read input yaml: %s')
-
-        module_path = os.path.dirname(os.path.abspath(__file__))
-        template_config_path = os.path.abspath(os.path.join(
-            module_path, template_path))
-
-        template_config = self._read_yaml(template_config_path, 'Sorry, can\'t read config template: %s')
+        app_config = CommentedYaml.read(
+            self.ctx, self.config, 'Sorry, can\'t read input yaml: %s')
+        template_config = CommentedYaml.read(
+            self.ctx, self.template_config_path(template_path), 'Sorry, can\'t read config template: %s')
 
         app_config[block_name] = template_config[block_name]
 
@@ -59,43 +54,44 @@ class CmdConfig(object):
                              (os.path.basename(self.config), backup_name))
             self.create_backup = False
 
-        self._write_yaml(self.config, app_config)
+        CommentedYaml.write(self.ctx, self.config, app_config)
 
         self.ctx.log('Configured %s with %s %s default parameters.' %
                      (os.path.basename(self.config), template_name, block_name))
 
-    def _write_yaml(self, filename, config):
-        try:
-            with open(filename, 'w') as out:
-                out.write(ruamel.yaml.dump(config, Dumper=ruamel.yaml.RoundTripDumper))
-        except EnvironmentError as e:   # parent of IOError, OSError *and* WindowsError where available
-            self.ctx.log('Sorry, can\'t write config: %s' % str(e))
-            sys.exit()
-
-    def _read_yaml(self, filename, error_message):
-        try:
-            with open(filename, 'r') as inp:
-                yaml = ruamel.yaml.load(inp, ruamel.yaml.RoundTripLoader)
-        except EnvironmentError as e:   # parent of IOError, OSError *and* WindowsError where available
-            self.ctx.log(error_message % str(e))
-            sys.exit()
-        return yaml
+    @staticmethod
+    def template_config_path(template):
+        module_path = os.path.dirname(os.path.abspath(__file__))
+        return os.path.abspath(os.path.join(module_path, template))
 
     @staticmethod
-    def list_configurations():
-        module_path = os.path.dirname(os.path.abspath(__file__))
-        template_config_path = os.path.abspath(os.path.join(
-            module_path, '../../templates/configs'))
-
+    def configurations():
         rv = []
-        for filename in os.listdir(template_config_path):
+        for filename in os.listdir(CmdConfig.template_config_path('../../templates/configs')):
             if filename.endswith('.yaml'):
                 rv.append(filename[:-5])
         rv.sort()
         return (rv, '|'.join(rv))
 
+    @staticmethod
+    def list_configurations(ctx):
+        ctx.log('Available algorithm configurations:\n')
+        templates = CmdConfig.template_config_path('../../templates/configs')
+        for filename in os.listdir(templates):
+            if filename.endswith('.yaml'):
+                ctx.log(click.style(filename[:-5], bold=True))
+                config = CommentedYaml.read(
+                    ctx, os.path.join(templates, filename), 'Sorry, can\'t read %s' % filename)
+                try:
+                    for comment_tokens in config.ca.comment:
+                        if comment_tokens:
+                            output = [comment.value for comment in comment_tokens]
+                            ctx.log(''.join(output).rstrip())
+                except:
+                    ctx.log('No description provided')
+        ctx.log('\nRun "relaax config --help" to see how to apply these configurations.\n')
 
-CONFIGURATIONS = CmdConfig.list_configurations()
+CONFIGURATIONS = CmdConfig.configurations()
 
 
 @click.command('config', short_help='Configure algorithm.')
@@ -118,6 +114,8 @@ def cmdl(ctx, config, algorithm, environment, local_algorithm):
     Fill algorithm or/and environment section of the configuration file with
     default parameters for the specified algorithm or/and environment.
     Old configuration file will be copied to a backup file.
+
+    Run "relaax config" without any options to see all available algorithm configurations.
     """
     ctx.setup_logger(format='')
     CmdConfig(ctx, config.name, algorithm=algorithm, environment=environment,
