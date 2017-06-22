483b3eaffdd6cf0e0c84b0237162414bb075fa98
skatedplrn <evgeny@dplrn.com>
2017-06-22 19:30:43 +0300

    PS. initialize_algorithm to PS.__init__

--- relaax/server/parameter_server/parameter_server_base.py
+++ relaax/server/parameter_server/parameter_server_base.py
@@ -1,7 +1,6 @@
 from builtins import object
 class ParameterServerBase(object):
     def __init__(self, saver_factory, metrics_factory):
-        self.initialize_algorithm()
         self.saver = saver_factory(self.make_checkpoint())
         self.metrics = metrics_factory(self.n_step)
 
@@ -15,9 +14,6 @@ class ParameterServerBase(object):
 
     def save_checkpoint(self):
         self.saver.save_checkpoint(self.n_step())
-
-    def initialize_algorithm(self):
-        raise NotImplementedError
 
     def make_checkpoint(self):
         raise NotImplementedError
