c2b9b3610aaa997a1bded3c864ec3bea49ea08ad
dennis <nemiroff.den@gmail.com>
2017-06-29 00:07:37 +0300

    upd: da3c episode wrt lstm state infer

--- relaax/algorithms/da3c/da3c_config.py
+++ relaax/algorithms/da3c/da3c_config.py
@@ -7,9 +7,10 @@ for key, value in [('use_convolutions', [])]:
         setattr(config, key, value)
 
 config.output.scale = options.get('algorithm/output/scale', 1.0)
+config.output.loss_type = options.get('algorithm/output/loss_type', 'Normal')
 config.optimizer = options.get('algorithm/optimizer', 'Adam')
 config.use_icm = options.get('algorithm/use_icm', False)
 config.use_gae = options.get('algorithm/use_gae', False)
-config.output.action_high = options.get('algorithm/output/action_high', 1.0)
-config.output.action_low = options.get('algorithm/output/action_low', -1.0)
+config.output.action_high = options.get('algorithm/output/action_high', [])
+config.output.action_low = options.get('algorithm/output/action_low', [])
 config.gradients_norm_clipping = options.get('algorithm/gradients_norm_clipping', False)
