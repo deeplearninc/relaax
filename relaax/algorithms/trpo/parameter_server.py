debc45349e0bb6681622bf26d9a150541940cfc4
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-06-30 18:42:06 +0300

    refactorings around ParameterServerBase and descendants.

--- relaax/algorithms/trpo/parameter_server.py
+++ relaax/algorithms/trpo/parameter_server.py
@@ -9,20 +9,11 @@ from .old_trpo_gae.parameter_server import parameter_server
 
 
 class ParameterServer(parameter_server_base.ParameterServerBase):
-    def close(self):
-        self.session.close()
-
-    def initialize_algorithm(self):
+    def init_session(self):
         sp = trpo_model.SharedParameters()
         self.session = session.Session(sp)
         sp._ps_bridge = parameter_server.ParameterServer(trpo_config.config, None, None, self.session).bridge()
         self.session.op_initialize()
-
-    def create_checkpoint(self):
-        return self.session.create_checkpoint()
-
-    def get_session(self):
-        return self.session
 
     def n_step(self):
         return self.session.op_n_step()
