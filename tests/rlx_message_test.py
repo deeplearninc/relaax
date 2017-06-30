0f0eb10875f06e4125a9a563e8ac777d0d963637
skatedplrn <evgeny@dplrn.com>
2017-06-30 15:59:44 +0300

    add more types

--- tests/rlx_message_test.py
+++ tests/rlx_message_test.py
@@ -20,6 +20,7 @@ class TestRLXMessage(object):
         data = {'arg1': 1, 'arg_1': -1, "args": "test1", "argn": -234, "argd": 0.067, "argbt": True, "argbf": False, "argnone": None,
                 "array_empty": [], 'array': [4.02, 5.2, 6.006],
                 'nparray1': npar1, 'nparray2': npar2, 'nparray1empty': npar1empty, 'nparray2empty': npar2empty,
+                'npint32': np.int32(555), 'npint64': np.int64(5559999),
                 'image_jpg': RLXMessageImage(imageJPG), 'image_bmp': RLXMessageImage(imageBMP)
                 }
         wire = RLXMessage.to_wire(data)
@@ -33,6 +34,12 @@ class TestRLXMessage(object):
                 assert isinstance(back[key], np.ndarray)
                 ndimage = np.asarray(data[key].image)
                 npt.assert_allclose(ndimage, back[key])
+            elif isinstance(data[key], np.int32):
+                assert int == type(back[key])
+                assert data[key] == back[key]
+            elif isinstance(data[key], np.int64):
+                assert int == type(back[key])
+                assert data[key] == back[key]
             else:
                 assert type(data[key]) == type(back[key])
                 assert data[key] == back[key]
