2cec95e9ca01cbd12af30fcb0e2aa1cf6ee44ac6
dennis <nemiroff.den@gmail.com>
2017-07-06 22:04:10 +0300

    init: agent & ps & episode

---
+++ relaax/algorithms/ddpg/parameter_server.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+from relaax.server.parameter_server import parameter_server_base
+from relaax.server.common import session
+
+from . import ddpg_model
+
+
+class ParameterServer(parameter_server_base.ParameterServerBase):
+    def __init__(self, saver_factory, metrics_factory):
+        self.session = session.Session(ddpg_model.SharedParameters())
+        self.session.op_initialize()
+        super(ParameterServer, self).__init__(saver_factory, metrics_factory)
+
+    def close(self):
+        self.session.close()
+
+    def create_checkpoint(self):
+        return self.session.create_checkpoint()
+
+    def get_session(self):
+        return self.session
+
+    def n_step(self):
+        return self.session.op_n_step()
