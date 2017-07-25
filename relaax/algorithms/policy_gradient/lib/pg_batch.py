59e060e866670bd9e1756b387028e9b54412ba91
skatedplrn <evgeny@dplrn.com>
2017-07-25 13:24:32 +0300

    rename log message

--- relaax/algorithms/policy_gradient/lib/pg_batch.py
+++ relaax/algorithms/policy_gradient/lib/pg_batch.py
@@ -35,7 +35,7 @@ class PGBatch(object):
         if reward is not None:
             self.push_experience(reward)
         if terminal and state is not None:
-            logger.warning('PGEpisode.step ignores state in case of terminal.')
+            logger.warning('PGBatch.step ignores state in case of terminal.')
             state = None
         else:
             assert state is not None
