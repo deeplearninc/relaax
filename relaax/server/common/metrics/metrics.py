fff3ccba9d687a6591c6ea329adc972d38aef04d
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-07-25 22:52:40 +0300

    Revert "Revert "support list of metrics in protocol""
    This reverts commit bdb91265e185282d16e4b79664cf66038d0f7d28.

--- relaax/server/common/metrics/metrics.py
+++ relaax/server/common/metrics/metrics.py
@@ -7,3 +7,7 @@ class Metrics(object):
 
     def histogram(self, name, y, x=None):
         raise NotImplementedError
+
+    def update(self, metrics):
+        for metric in metrics:
+            getattr(self, metric['method'])(name=metric['name'], y=metric['y'], x=metric['x'])
