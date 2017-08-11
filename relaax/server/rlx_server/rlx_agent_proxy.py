f340681dcf41db5d585f68b3eafcb8b54fe2f574
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-08-11 20:39:15 +0300

    relaax/server/rlx_server/rlx_agent_proxy.py: average reward

--- relaax/server/rlx_server/rlx_agent_proxy.py
+++ relaax/server/rlx_server/rlx_agent_proxy.py
@@ -20,6 +20,7 @@ class RLXAgentProxy(object):
         metrics_connection = metrics_bridge_connection.MetricsBridgeConnection(options.metrics_server)
         metrics = x_metrics.XMetrics(ps.session.op_n_step, metrics_connection.metrics)
         self.agent = options.Agent(ps, metrics)
+        self.average = Average(100, lambda x: self.agent.metrics.scalar('average_reward', x))
 
     def init(self, data):
         exploit = data['exploit'] if 'exploit' in data else False
@@ -29,11 +30,10 @@ class RLXAgentProxy(object):
             return self._error_message('can\'t initialize agent')
 
     def update(self, data):
-        action = self.agent.update(
-            reward=data['reward'],
-            state=data['state'],
-            terminal=data['terminal']
-        )
+        reward = data['reward']
+        if reward is not None:
+            self.average.append(reward)
+        action = self.agent.update(reward=reward, state=data['state'], terminal=data['terminal'])
         #if isinstance(action, np.ndarray):
         #    action = np.asarray(action).tolist()
         return {'response': 'action', 'data': action}
@@ -61,3 +61,19 @@ class RLXAgentProxy(object):
 
     def _error_message(self, message):
         return {'response': 'error', 'message': message}
+
+
+class Average(object):
+    def __init__(self, n, cb):
+        self._cb = cb
+        self._n = n
+        self._i = 0
+        self._sum = 0.
+
+    def append(self, v):
+        self._i += 1
+        self._sum += v
+        if self._i >= self._n:
+            self._cb(self._sum / self._i)
+            self._i = 0
+            self._sum = 0.
