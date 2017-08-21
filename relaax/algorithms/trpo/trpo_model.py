76320d111b109444e006292e5366bf62feff5a94
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-08-21 18:49:56 +0300

    relaax/algorithms/trpo/trpo_model.py: reformat

--- relaax/algorithms/trpo/trpo_model.py
+++ relaax/algorithms/trpo/trpo_model.py
@@ -16,8 +16,7 @@ from .lib import trpo_graph
 class GetVariablesFlatten(subgraph.Subgraph):
     def build_graph(self, variables):
         tensors = utils.Utils.flatten(variables.node)
-        return tf.concat([tf.reshape(t, [np.prod(t.shape.as_list())])
-            for t in tensors], axis=0)
+        return tf.concat([tf.reshape(t, [np.prod(t.shape.as_list())]) for t in tensors], axis=0)
 
 
 class SetVariablesFlatten(subgraph.Subgraph):
@@ -30,8 +29,7 @@ class SetVariablesFlatten(subgraph.Subgraph):
         for t in tensors:
             shape = t.shape.as_list()
             size = np.prod(shape)
-            assignes.append(tf.assign(t,
-                tf.reshape(self.ph_value[start:start + size], shape)))
+            assignes.append(tf.assign(t, tf.reshape(self.ph_value[start:start + size], shape)))
             start += size
         assert start == total_size
         return tf.group(*assignes)
@@ -48,13 +46,11 @@ class Categorical(subgraph.Subgraph):
 
     class LoglikelihoodSubgraph(subgraph.Subgraph):
         def build_graph(self, prob, sampled_variable, n):
-            return tf.log(tf.reduce_sum(prob.node *
-                tf.one_hot(sampled_variable.node, n), axis=1))
+            return tf.log(tf.reduce_sum(prob.node * tf.one_hot(sampled_variable.node, n), axis=1))
 
     class KlSubgraph(subgraph.Subgraph):
         def build_graph(self, prob0, prob1):
-            return tf.reduce_sum(prob0.node *
-                tf.log(prob0.node / prob1.node), axis=1)
+            return tf.reduce_sum(prob0.node * tf.log(prob0.node / prob1.node), axis=1)
 
     class EntropySubgraph(subgraph.Subgraph):
         def build_graph(self, prob):
@@ -66,8 +62,7 @@ class Categorical(subgraph.Subgraph):
 
     @property
     def Loglikelihood(self):
-        return lambda prob: self.LoglikelihoodSubgraph(
-                prob, self.ph_sampled_variable, self._n)
+        return lambda prob: self.LoglikelihoodSubgraph(prob, self.ph_sampled_variable, self._n)
 
     @property
     def Kl(self):
@@ -91,8 +86,8 @@ class DiagGauss(subgraph.Subgraph):
         def build_graph(self, prob, sampled_variable, d):
             mean0 = prob.node[:, :d]
             std0 = prob.node[:, d:]
-            return - 0.5 * tf.reduce_sum(tf.square((sampled_variable.node - mean0) /
-                    std0), axis=1) - 0.5 * tf.log(2.0 * np.pi) * d - tf.reduce_sum(tf.log(std0), axis=1)
+            return - 0.5 * tf.reduce_sum(tf.square((sampled_variable.node - mean0) / std0), axis=1) \
+                   - 0.5 * tf.log(2.0 * np.pi) * d - tf.reduce_sum(tf.log(std0), axis=1)
 
     class KlSubgraph(subgraph.Subgraph):
         def build_graph(self, prob0, prob1, d):
@@ -115,8 +110,7 @@ class DiagGauss(subgraph.Subgraph):
 
     @property
     def Loglikelihood(self):
-        return lambda prob: self.LoglikelihoodSubgraph(
-                prob, self.ph_sampled_variable, self._d)
+        return lambda prob: self.LoglikelihoodSubgraph(prob, self.ph_sampled_variable, self._d)
 
     @property
     def Kl(self):
@@ -149,8 +143,8 @@ class Network(subgraph.Subgraph):
         input = layer.Input(trpo_config.config.input)
 
         head = layer.GenericLayers(layer.Flatten(input),
-                [dict(type=layer.Dense, size=size, activation=layer.Activation.Tanh)
-                for size in trpo_config.config.hidden_sizes])
+                                   [dict(type=layer.Dense, size=size, activation=layer.Activation.Tanh)
+                                    for size in trpo_config.config.hidden_sizes])
 
         if trpo_config.config.output.continuous:
             output = layer.Dense(head, trpo_config.config.output.action_size)
@@ -158,13 +152,12 @@ class Network(subgraph.Subgraph):
             actor_layers = [output, actor]
         else:
             actor = layer.Dense(head, trpo_config.config.output.action_size,
-                    activation=layer.Activation.Softmax)
+                                activation=layer.Activation.Softmax)
             actor_layers = [actor]
 
         self.ph_state = input.ph_state
         self.actor = actor
         self.weights = layer.Weights(*([input, head] + actor_layers))
-
 
 
 # Weights of the policy are shared across
@@ -201,8 +194,8 @@ class SharedParameters(subgraph.Subgraph):
         sg_oldlogp_n = sg_probtype.Loglikelihood(ph_oldprob_np)
 
         # Policy gradient:
-        sg_surr = graph.TfNode(-tf.reduce_mean(tf.multiply(tf.exp(sg_logp_n.node -
-            sg_oldlogp_n.node), ph_adv_n.node)))
+        sg_surr = graph.TfNode(-tf.reduce_mean(tf.multiply(tf.exp(sg_logp_n.node - sg_oldlogp_n.node),
+                                                           ph_adv_n.node)))
         sg_gradients = optimizer.Gradients(sg_network.weights, loss=sg_surr)
         sg_gradients_flatten = GetVariablesFlatten(sg_gradients.calculate)
 
@@ -234,14 +227,14 @@ class SharedParameters(subgraph.Subgraph):
         self.op_get_action = self.Op(sg_network.actor, state=sg_network.ph_state)
 
         self.op_get_weights_flatten = self.Op(sg_get_weights_flatten)
-        self.op_set_weights_flatten = self.Op(sg_set_weights_flatten, weights=sg_set_weights_flatten.ph_value)
+        self.op_set_weights_flatten = self.Op(sg_set_weights_flatten,
+                                              weights=sg_set_weights_flatten.ph_value)
 
         self.op_get_weights = self.Op(sg_network.weights)
 
-        self.op_compute_policy_gradient = self.Op(sg_gradients_flatten,
-                state=sg_network.ph_state, sampled_variable=self.sampled_variable,
-                adv_n=ph_adv_n, oldprob_np=ph_oldprob_np)
-
+        self.op_compute_policy_gradient = self.Op(sg_gradients_flatten, state=sg_network.ph_state,
+                                                  sampled_variable=self.sampled_variable, adv_n=ph_adv_n,
+                                                  oldprob_np=ph_oldprob_np)
 
 
 # Policy run by Agent(s)
