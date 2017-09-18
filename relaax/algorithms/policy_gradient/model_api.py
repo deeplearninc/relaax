b4853afd824a6751a6d5c08f04288ce40f3268e4
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-09-18 21:59:42 +0300

    relaax/algorithms/policy_gradient: make flake8 happy

--- relaax/algorithms/policy_gradient/model_api.py
+++ relaax/algorithms/policy_gradient/model_api.py
@@ -14,33 +14,26 @@ logger = logging.getLogger(__name__)
 # stop updating shared parameters and at the end of every episode load
 # new policy parameters from PS
 class ModelApi(object):
-  def __init__(self, session_arg, exploit, parameter_server):
-    self.ps = parameter_server
-    self.exploit = exploit
-    self.session = session_arg
+    def __init__(self, session_arg, exploit, parameter_server):
+        self.ps = parameter_server
+        self.exploit = exploit
+        self.session = session_arg
 
-  def load_shared_parameters(self):
-    self.session.op_assign_weights(weights=self.ps.session.op_get_weights())
+    def load_shared_parameters(self):
+        self.session.op_assign_weights(weights=self.ps.session.op_get_weights())
 
-  def action_from_policy(self, state):
-    assert state is not None
-    state = np.asarray(state)
-    state = np.reshape(state, (1, ) + state.shape)
-    probabilities, = self.session.op_get_action(state=state)
-    return utils.choose_action_descrete(probabilities, self.exploit)
+    def action_from_policy(self, state):
+        assert state is not None
+        state = np.asarray(state)
+        state = np.reshape(state, (1, ) + state.shape)
+        probabilities, = self.session.op_get_action(state=state)
+        return utils.choose_action_descrete(probabilities, self.exploit)
 
-  def compute_gradients(self, experience_arg):
-    discounted_reward = utils.discounted_reward(
-      experience_arg['reward'],
-      pg_config.config.GAMMA
-    )
-    return self.session.op_compute_gradients(
-      state=experience_arg['state'],
-      action=experience_arg['action'],
-      discounted_reward=discounted_reward
-    )
+    def compute_gradients(self, experience_arg):
+        discounted_reward = utils.discounted_reward(experience_arg['reward'], pg_config.config.GAMMA)
+        return self.session.op_compute_gradients(state=experience_arg['state'],
+                                                 action=experience_arg['action'],
+                                                 discounted_reward=discounted_reward)
 
-  def apply_gradients(self, gradients, size):
-    self.ps.session.op_apply_gradients(gradients=gradients, increment=size)
-
-
+    def apply_gradients(self, gradients, size):
+        self.ps.session.op_apply_gradients(gradients=gradients, increment=size)
