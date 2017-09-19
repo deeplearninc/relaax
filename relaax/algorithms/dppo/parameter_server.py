dc04b330036e254c29c9899195af75e43e1b9cd4
Alexander Fishkov <jetsnguns@gmail.com>
2017-09-19 18:29:56 +0300

    fix single session.

--- relaax/algorithms/dppo/parameter_server.py
+++ relaax/algorithms/dppo/parameter_server.py
@@ -10,15 +10,17 @@ class ParameterServer(parameter_server_base.ParameterServerBase):
         policy_shared = dppo_model.SharedWeights(dppo_model.Network().weights)
         value_func_shared = dppo_model.SharedWeights(dppo_model.ValueNet().weights)
 
-        self.policy_session = session.Session(policy_shared)
+        """self.policy_session = session.Session(policy_shared)
         self.policy_session.op_initialize()
         self.policy_session.op_init_weight_history()
 
         self.value_session = session.Session(value_func_shared)
         self.value_session.op_initialize()
-        self.value_session.op_init_weight_history()
+        self.value_session.op_init_weight_history()"""
+
+        self.session = session.Session(policy=policy_shared, value_func=value_func_shared)
 
     def n_step(self):
-        policy_step = self.policy_session.op_n_step()
-        value_step = self.value_session.op_n_step()
+        policy_step = self.session.policy.op_n_step()
+        value_step = self.session.value_func.op_n_step()
         return policy_step
