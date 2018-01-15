7c08c89f6d998326467334b0621267383a2f4128
4SkyNet <nemiroff.den@gmail.com>
2018-01-15 04:25:40 +0300

    fit: #3 cmdl changes wrt flake8

--- relaax/cmdl/commands/cmd_config.py
+++ relaax/cmdl/commands/cmd_config.py
@@ -68,7 +68,7 @@ class CmdConfig(object):
                                  self.algorithm, env_section['type'], env_section['name'])
                     self.ctx.log('WARNING: this is NOT equal to requested %s environment type',
                                  self.environment)
-            except Exception:
+            except KeyError:
                 pass
 
             app_config[block_name] = template_config[block_name]
